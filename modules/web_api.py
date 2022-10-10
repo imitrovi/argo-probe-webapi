e218a2c508ebb95eb73b29704c829cbb041cc1a9
Dean Hudek <hudek.dean@gmail.com>
2022-10-10 15:56:20 +0200

    tests added

--- modules/web_api.py
+++ modules/web_api.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import requests
 import argparse
 import datetime
@@ -13,48 +12,7 @@ strerr = ''
 num_excp_expand = 0
 server_expire = None
 NAGIOS_RESULT = 0
-
-
-def errmsg_from_excp(e):
-    global strerr, num_excp_expand
-    if isinstance(e, Exception) and getattr(e, 'args', False):
-        num_excp_expand += 1
-        if not errmsg_from_excp(e.args):
-            return strerr
-    elif isinstance(e, dict):
-        for s in e.iteritems():
-            errmsg_from_excp(s)
-    elif isinstance(e, list):
-        for s in e:
-            errmsg_from_excp(s)
-    elif isinstance(e, tuple):
-        for s in e:
-            errmsg_from_excp(s)
-    elif isinstance(e, str):
-        if num_excp_expand <= 5:
-            strerr += e + ' '
-    elif isinstance(e, int):
-        if num_excp_expand <= 5:
-            strerr += str(e) + ' '
-
-
-def checkUnusedReports(arguments, reportName):
-    """ We have a number of reports that are not used anymore.
-        In order not to check these reports we define which one
-        to ignore
-        Args:
-            reportName: the name of the report to check
-            arguments: the input arguments
-    """
-    Reports = ''
-    #reports we dont use in production
-    if arguments.unusedreports:
-        Reports = arguments.unusedreports
-
-    if Reports!='':
-        if reportName in Reports:
-           return 1
-
+   
 
 def createAPICallUrl(arguments):
     """Create the main API Call Reports Url to get the
@@ -69,25 +27,23 @@ def createAPICallUrl(arguments):
 
         #make the call to get the json
         profiles = requests.get('https://' + arguments.hostname + API_REPORTS, headers = headers,timeout=arguments.timeout)
+        
+        profiles.raise_for_status()
         profilesjson = profiles.json()
 
-        #raise an HTTPError exception for non 200 status codes
-        if profiles.status_code != 200:
-           profiles.raise_for_status()
-    #check for connection request
-    except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
-        print ('CRITICAL - API cannot connect to %s: %s' % 'https://' + arguments.hostname + API_REPORTS,
-                                                       errmsg_from_excp(e))
-        print ('CRITICAL - status code error. Cannot connect to %s: %s' % 'https://' + arguments.hostname + API_REPORTS,
-                                                       errmsg_from_excp(e))
-        raise SystemExit(2)
 
+    #check for connection request
+    except (requests.exceptions.RequestException, requests.exceptions.HTTPError):
+        print ('CRITICAL - API cannot connect to https://' + arguments.hostname + API_REPORTS)
+
+        raise SystemExit(2)
 
     # Remove disabled reports from response's data results
     profilesjson["data"] = [ x for x in profilesjson["data"] if x["disabled"] == False ]
-
-
+     
     return profilesjson
+
+
 
 
 def convert_date(year,month,day,daytype):
@@ -99,9 +55,9 @@ def convert_date(year,month,day,daytype):
            day: the day
            daytype: construct the start or end date for the api call
     """
-    orig_date = datetime.datetime(year,month,day)
-    orig_date = str(orig_date)
+    orig_date = str(datetime.datetime(year,month,day))
     d = datetime.datetime.strptime(orig_date, '%Y-%m-%d %H:%M:%S')
+
     if (daytype=='start'):
         d = d.strftime('%Y-%m-%dT00:00:00Z')
     else:
@@ -125,22 +81,20 @@ def CheckResults(arguments, profilesjson):
        pathToUse = API_RESULTS
 
     a = datetime.datetime.today()
+    
     timeBackCheck = arguments.day
     yesterday = a - timedelta(timeBackCheck)
+    
     start = convert_date(yesterday.year, yesterday.month, yesterday.day, 'start')
     end = convert_date(yesterday.year, yesterday.month, yesterday.day, 'end')
     ProbeDescription = {}
+    
     debug = ""
 
     #get all report names
     while (count < allReports):
         reportName = profilesjson['data'][count]['info']['name']
         reportTopologyGroup = profilesjson['data'][count]['topology_schema']['group']['group']['type']
-
-        #if we dont use this report anymore just continue
-        if checkUnusedReports(arguments, reportName)==1:
-            count+=1
-            continue;
 
         #create the main headers for json call
         try:
@@ -151,18 +105,19 @@ def CheckResults(arguments, profilesjson):
             else:
                 urlToAPI = 'https://' + arguments.hostname + pathToUse+'/' +reportName+'/'+reportTopologyGroup+'?start_time='+start+'&end_time='+end
             resultsAPI = requests.get(urlToAPI, headers = headers, timeout=arguments.timeout)
+            
             count+=1;
             if arguments.debug:
                 debug = debug +  "[CheckResults] API call:"+ urlToAPI
             resultsjson = resultsAPI.json()
-
+            
             #raise an HTTPError exception for non 200 status codes
             if resultsAPI.status_code != 200:
                 resultsAPI.raise_for_status()
         #check for connection request
-        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
-            ProbeDescription['Connection'+reportName] = 'CRITICAL - Reports cannot connect to (https://' + arguments.hostname + pathToUse+'/' +reportName+'/'+reportTopologyGroup +'-'+ errmsg_from_excp(e)
-            ProbeDescription['Status'+reportName] = 'CRITICAL - Status code error. Cannot connect to (https://' + arguments.hostname + pathToUse+'/' +reportName+'/'+reportTopologyGroup +'-'+ errmsg_from_excp(e)
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError):
+            ProbeDescription['Connection'+reportName] = 'CRITICAL - Reports cannot connect to (https://' + arguments.hostname + pathToUse+'/' +reportName+'/'+reportTopologyGroup+')'
+            ProbeDescription['Status'+reportName] = 'CRITICAL - Status code error. Cannot connect to (https://' + arguments.hostname + pathToUse+'/' +reportName+'/'+reportTopologyGroup+')'
 
 
         #when we have results report
@@ -199,6 +154,31 @@ def debugValues(arguments):
             print ("[debugValues] - timeout:" + str(arguments.timeout))
 
 
+def utils(arguments, output_dict):
+    Description = ''
+    if arguments.rtype not in ('status', 'ar'):
+        NAGIOS_RESULT = 2
+        msg = "CRITICAL: wrong value at argument rtype. rtype must be ar or status"
+    
+    else:   
+        for item in output_dict:
+            if "WARNING" in output_dict[item]: 
+                msg = "SystemExit(1)"
+                NAGIOS_RESULT = 1
+            elif "CRITICAL" in output_dict[item]:
+                msg = f'CRITICAL - Problem with {arguments.rtype} reports for tenant {arguments.tenant} return results'
+                NAGIOS_RESULT = 2
+            else:
+                NAGIOS_RESULT = 0
+                msg = f'Ok - All {arguments.rtype} reports for tenant {arguments.tenant} return results'
+            Description += output_dict[item] + "\n"
+
+    print(msg)
+    print(f"Description: {Description}")
+    
+    raise SystemExit(NAGIOS_RESULT)
+   
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-H', dest='hostname', required=True, type=str, help='hostname')
@@ -210,39 +190,13 @@ def main():
     parser.add_argument('-t', dest='timeout', required=False, type=int, default=180)
     parser.add_argument('-v', dest='debug', help='Set verbosity level', action='count', default=0)
     arguments = parser.parse_args()
-    NAGIOS_RESULT = 0
 
-    #check arguments
-    if arguments.rtype not in ('status','ar'):
-        NAGIOS_RESULT = 2
-        print ('CRITICAL: wrong value at argument rtype. rtype must be ar or status')
-        raise SystemExit(2)
+    profilejson = createAPICallUrl(arguments)
+    data, debugData = CheckResults(arguments, profilejson)
 
-    profilesjson = createAPICallUrl(arguments)
-    data,debugData = CheckResults(arguments, profilesjson)
-    Description = ''
-    for item in data:
-        if 'WARNING' in data[item]:
-            NAGIOS_RESULT = 1
-        if 'CRITICAL' in data[item]:
-            NAGIOS_RESULT = 2
-        Description = Description + data[item] + "\n"
+    utils(arguments, output_dict=data)
 
-    if NAGIOS_RESULT == 0:
-        print ('OK - All %s reports for tenant %s return results ' % (arguments.rtype, arguments.tenant))
-        print ('Description:'+Description)
-        debugValues(arguments)
-        raise SystemExit(0)
-    elif NAGIOS_RESULT == 1:
-        print (debugData)
-        debugValues(arguments)
-        raise SystemExit(1)
-    elif NAGIOS_RESULT == 2:
-        print ('CRITICAL - Problem with  %s reports for tenant %s return results  ' % (arguments.rtype, arguments.tenant))
-        print ('Description:'+Description)
-        print (debugData)
-        debugValues(arguments)
-        raise SystemExit(2)
+
 
 if __name__ == "__main__":
     main()
